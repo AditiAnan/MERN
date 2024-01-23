ecb02b0d1cc4a6f7bd736f6e3ce46c47501827aa
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-01-22 23:36:31 -0500

    Made sign in and cart header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container, NavbarCollapse } from 'react-bootstrap'
+import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="md" collaspeOnSelect>
+         <Container>
+           <Navbar.Brand href="/">ProShop</Navbar.Brand>
+           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+           <NavbarCollapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+                <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                <Nav.Link href="/login"><FaUser /> Sign In </Nav.Link>
+            </Nav>
+           </NavbarCollapse>
+
+         </Container>
+        </Navbar>
+     </header>
+  )
+}
+
+export default Header
