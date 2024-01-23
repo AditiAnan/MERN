a8710a0b0ccbef25e4fb28fff93c9cc58bc2400c
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-01-23 17:47:16 -0500

    Fixed Product rafce naming

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import Rating from './Rating';
 
-const Products = ({product}) => {
+const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
@@ -29,4 +29,4 @@ const Products = ({product}) => {
   );
 };
 
-export default Products
+export default Product
