e7c02b72a73df7fc7323609c199de022d46620bb
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-02-05 01:13:11 -0500

    register page

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,7 +1,7 @@
 import { apiSlice } from './apiSlice';
 import { USERS_URL } from '../constants';
 
-export const userApiSlice = apiSlice.injectEndpoints({
+export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
@@ -10,7 +10,20 @@ export const userApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    register: builder.mutation({
+        query: (data) => ({
+          url: `${USERS_URL}`,
+          method: 'POST',
+          body: data,
+        }),
+      }),
+    logout: builder.mutation({
+        query: () => ({
+          url: `${USERS_URL}/logout`,
+          method: 'POST',
+        }),
+      }),
   }),
 });
 
-export const { useLoginMutation } = userApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
