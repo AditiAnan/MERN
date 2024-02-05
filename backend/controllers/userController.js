6e4de28d21a0a14344b513e85ad715a4e9c08a59
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-02-04 20:57:33 -0500

    user and password auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,22 @@ import User from '../models/userModel.js';
 
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-  res.send('auth user');
+
+    const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 
