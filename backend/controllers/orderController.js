09e58e1cb9082910936a247212e6240f3983b781
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-02-16 03:17:44 -0500

    updating order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,27 +1,65 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Order from '../models/orderModel.js';
 
+const addOrderItems = asyncHandler(async(req, res) => {
+    const { 
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice, 
+    } = req.body;
 
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-const addOrderItems = asyncHandler(async (req, res) => {
-  res.send('add order item');
-});
+    const createdOrder = await order.save();
+ 
+    res.status(201).json(createdOrder);
+}});
+
 
 
 const getMyOrders = asyncHandler(async (req, res) => {
-    res.send('get my item');
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 
 const getOrderById = asyncHandler(async (req, res) => {
-    res.send('get order by id');
-
-});
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+      );
+    
+      if (order) {
+        res.json(order);
+      } else {
+        res.status(404);
+        throw new Error('Order not found');
+      }
+    });
 
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
     res.send('update order to paid');
-
 });
 
 
@@ -32,7 +70,7 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 
 
 const getOrders = asyncHandler(async (req, res) => {
-    res.send('update order to paid');
+    res.send('get all');
 
 });
 
