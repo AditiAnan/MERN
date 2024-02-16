925425960e2d85b43da09879cc01f46f42a771a2
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-02-16 01:04:01 -0500

    private routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,7 +19,8 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen'; 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
+import ShippingScreen from './screens/ShippingScreen'; 
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,7 +30,12 @@ const router = createBrowserRouter(
   <Route path='/cart' element={<CartScreen />} />
   <Route path='/login' element={<LoginScreen />} />
   <Route path='/register' element={<RegisterScreen />} />
-  <Route path='/shipping' element={<ShippingScreen />} />
+  
+
+  <Route path='' element={<PrivateRoute />}>
+   <Route path='/shipping' element={<ShippingScreen />} />
+
+  </Route>
 
 
 
