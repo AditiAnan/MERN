3ba1e1945ae3ebbd393602fa10093619cfaaa162
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-02-16 02:37:06 -0500

    trying to fix 401 error

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 
 const port = process.env.PORT || 5000;
 
