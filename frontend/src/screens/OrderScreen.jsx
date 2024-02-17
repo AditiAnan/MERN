7b330e4c68220708590413839d51e5ace44e3d5b
Aditi Anandjiwala <aditianandjiwala10@gmail.com>
2024-02-16 21:44:08 -0500

    order screen

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import {
+  useGetOrderDetailsQuery,
+} from '../slices/ordersApiSlice';
+
+const OrderScreen = () => {
+  const { id: orderId } = useParams();
+
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    error,
+  } = useGetOrderDetailsQuery(orderId);
+
+  return<div>OrderScreen</div>;
+};
+export default OrderScreen;
