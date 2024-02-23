557cb8f9f204efdf1ea4c650a3e8b4385300535a
AditiAnan <aditianandjiwala10@gmail.com>
2024-02-23 04:06:11 -0500

    updates order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -11,13 +11,12 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('No order items');
   } else {
- 
-    // get the ordered items from our database
+   
+
     const itemsFromDB = await Product.find({
       _id: { $in: orderItems.map((x) => x._id) },
     });
 
-    // map over the order items and use the price from our items from database
     const dbOrderItems = orderItems.map((itemFromClient) => {
       const matchingItemFromDB = itemsFromDB.find(
         (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
@@ -52,42 +51,79 @@ const addOrderItems = asyncHandler(async (req, res) => {
 });
 
 
-
 const getMyOrders = asyncHandler(async (req, res) => {
-    const orders = await Order.find({ user: req.user._id });
-    res.status(200).json(orders);
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
 });
 
 
 const getOrderById = asyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate(
-        'user',
-        'name email'
-        );
-    
-      if (order) {
-        res.status(200).json(order);
-      } else {
-        res.status(404);
-        throw new Error('Order not found');
-      }
-    });
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
 
 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-    res.send('update order to paid');
+
+  const { verified, value } = await verifyPayPalPayment(req.body.id);
+  if (!verified) throw new Error('Payment not verified');
+
+  const isNewTransaction = await checkIfNewTransaction(Order, req.body.id);
+  if (!isNewTransaction) throw new Error('Transaction has been used before');
+
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    const paidCorrectAmount = order.totalPrice.toString() === value;
+    if (!paidCorrectAmount) throw new Error('Incorrect amount paid');
+
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-    res.send('update order to deliver');
+  const order = await Order.findById(req.params.id);
 
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
-
 const getOrders = asyncHandler(async (req, res) => {
-    res.send('get all');
-
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.json(orders);
 });
 
 export {
