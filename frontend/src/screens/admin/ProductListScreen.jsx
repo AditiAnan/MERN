901e086c003ba9da292e2e1e3a1bd6fc8765597c
AditiAnan <aditianandjiwala10@gmail.com>
2024-02-24 11:18:51 -0500

    edits products

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,15 +1,18 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
-import { FaEdit, FaTimes, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
 import {
-  useGetProductsQuery,
+  useGetProductsQuery, useCreateProductMutation
 } from '../../slices/productsApiSlice';
+import { toast } from 'react-toastify';
+
+
 
 const ProductListScreen = () => {
 
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
 
 
   const deleteHandler =  (id) => {
@@ -17,7 +20,20 @@ console.log(`delete`, id);
     
   };
 
-  
+  const [createProduct, { isLoading: loadingCreate }] =
+  useCreateProductMutation();
+
+const createProductHandler = async () => {
+  if (window.confirm('Are you sure you want to create a new product?')) {
+    try {
+      await createProduct();
+      refetch();
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  }
+};
+
 
   return (
     <>
@@ -26,7 +42,7 @@ console.log(`delete`, id);
           <h1>Products</h1>
         </Col>
         <Col className='text-end'>
-          <Button className="btn-sm m-3" >
+          <Button className="btn-sm m-3" onClick={ createProductHandler} >
             <FaEdit /> Create Product
           </Button>
         </Col>
